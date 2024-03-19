fb36f2df526eeb63921901d4b6afea6c8a74e4ac
PraaneshSelvaraj <praaneshselvaraj2003@gmail.com>
2024-03-19 16:49:39 +0530

    Get Logs Excel

--- controllers/logsController.py
+++ controllers/logsController.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Response
 from filters import responseFilter, requestFilter
 from services import logsService
 
@@ -7,3 +7,7 @@ router = APIRouter()
 @router.get("/logs", status_code=status.HTTP_200_OK, response_model=responseFilter.Response)
 async def get_logs(query : requestFilter.LogQuery):
     return await logsService.get_logs(query)
+
+@router.get("/logs/excel", response_class=Response, status_code=200)
+async def get_logs_excel(query : requestFilter.LogQuery):
+    return await logsService.get_logs_excel(query)
--- services/logsService.py
+++ services/logsService.py
@@ -1,8 +1,28 @@
 from databases import mongoDB
 from filters import requestFilter
+import pandas as pd
+import io
+from fastapi.responses import StreamingResponse
 
 mongoUtil = mongoDB.MongoUtil()
 
 async def get_logs(query : requestFilter.LogQuery):
     logs = mongoUtil.get_logs(query.start_date, query.end_date)
     return {"message" : "Logs", "data" : {"logs" : logs}}
+
+async def get_logs_excel(query: requestFilter.LogQuery):
+    logs = mongoUtil.get_logs(query.start_date, query.end_date)
+
+    df = pd.DataFrame(logs)
+
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    excel_output = io.BytesIO()
+    df.to_excel(excel_output, index=False)
+    excel_output.seek(0)
+
+    return StreamingResponse(
+        iter([excel_output.getvalue()]),
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        headers={"Content-Disposition": "attachment;filename=logs_report.xlsx"}
+    )
